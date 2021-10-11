f3247a79abc0da56cdee872c6464710e41247f80
ruxiliang <91934643+ruxiliang@users.noreply.github.com>
2021-10-11 21:05:49 +0800

    Update SchemeFrame.kt

--- kotlin/src/main/kotlin/org/ruxi/scheme/environment/SchemeFrame.kt
+++ kotlin/src/main/kotlin/org/ruxi/scheme/environment/SchemeFrame.kt
@@ -4,9 +4,7 @@ import org.ruxi.scheme.types.SchemeExpr
 import org.ruxi.scheme.types.SchemeSymbol
 import org.ruxi.scheme.types.SchemeType
 
-/**
- * 需要注意的是，由于builtin函数不存builtin的参数名，所以为了偷懒这边frame多开了个存builtin实参的field
- */
+
 data class SchemeFrame(val currEnv:MutableMap<SchemeSymbol,SchemeExpr>,val parentEnv:SchemeFrame?)
 
 fun SchemeFrame.extendEnv(curr:MutableMap<SchemeSymbol,SchemeExpr>):SchemeFrame = SchemeFrame(curr,this)
